df4f558c3904f2b0b360879294fda1de0275a051
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-03-19 12:03:44 +0000

    removed references to mongo in config file - SMS still sends

--- config.js
+++ config.js
@@ -12,11 +12,11 @@ cfg.authToken = process.env.TWILIO_AUTH_TOKEN;
 
 cfg.twilioNumber = process.env.TWILIO_NUMBER;
 
-cfg.mongoUrl =
-  process.env.MONGOLAB_URI ||
-  process.env.MONGO_URL ||
-  'mongodb://localhost:27017';
+// cfg.mongoUrl =
+//   process.env.MONGOLAB_URI ||
+//   process.env.MONGO_URL ||
+//   'mongodb://localhost:27017';
 
-cfg.mongoUrlTest = 'mongodb://localhost:8000';
+// cfg.mongoUrlTest = 'mongodb://localhost:8000';
 
 module.exports = cfg;
